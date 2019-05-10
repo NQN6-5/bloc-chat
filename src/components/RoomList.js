523e0d2395ce1888998aee0017b8af6e8b92eeac
NQN6-5 <dcastoral@outlook.com>
2019-05-10 11:22:05 -0400

    compiled room list

--- src/components/RoomList.js
+++ src/components/RoomList.js
@@ -13,7 +13,6 @@ class RoomList extends Component {
 
   componentDidMount() {
      this.roomsRef.on('child_added', snapshot => {
-       this.setState({ rooms: this.state.rooms.concat( snapshot.val() ) });
        const room = snapshot.val();
        room.key = snapshot.key;
        this.setState({ rooms: this.state.rooms.concat( room ) })
